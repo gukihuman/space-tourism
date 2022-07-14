51267e19804828c0564005bfe6cf013aa8ce62c8
ivan-f-sokolov <sokolovivanf@gmail.com>
2022-07-14 19:40:39 +0500

    Desktop destination styling

--- src/views/destination/DestinationView.vue
+++ src/views/destination/DestinationView.vue
@@ -1,28 +1,32 @@
 <template lang="pug">
 
-div(class="flex flex-col")
-  p(class="font-barlow-condensed text-white text-base leading-[19px] \
-  tracking-[2.7px] flex justify-center mt-[10px] sm:text-xl sm:leading-6 \
-  sm:justify-start sm:mt-10 sm:ml-[38.5px] sm:tracking-[3.38px]")
-    span(class="opacity-25 mr-[18px] font-bold sm:mr-[19px]") 01
-    span PICK YOUR DESTINATION
+div(class="flex flex-col xl:flex-row")
+  div(class="xl:ml-[166px] xl:mt-[76px] xl:w-[509px] xl:h-[576px] xl:flex \
+  xl:justify-between flex-col")
+    p(class="font-barlow-condensed text-white text-base leading-[19px] \
+    tracking-[2.7px] flex justify-center mt-[10px] sm:text-xl sm:leading-6 \
+    sm:justify-start sm:mt-10 sm:ml-[38.5px] sm:tracking-[3.38px] \
+    xl:text-[28px] xl:leading-[33.6p] xl:tracking-[4.72px] \
+    xl:ml-0 xl:mt-0")
+      span(class="opacity-25 mr-[18px] font-bold sm:mr-[19px] xl:mr-7") 01
+      span PICK YOUR DESTINATION
 
-  picture
-    source(
-      :srcset="require(`@/assets/destination/${$route.params.spaceObject}.webp`)"
-      type="image/webp"
-      )
-    source(
-      :srcset="require(`@/assets/destination/${$route.params.spaceObject}.png`)"
-      type="image/png"
-      )
-    img(:alt="$route.params.spaceObject" class="h-[170px] mx-auto mt-8 \
-    sm:h-[300px] sm:mt-[60px]")
+    picture(class="w-fit xl:self-end")
+      source(
+        :srcset="require(`@/assets/destination/${$route.params.spaceObject}.webp`)"
+        type="image/webp"
+        )
+      source(
+        :srcset="require(`@/assets/destination/${$route.params.spaceObject}.png`)"
+        type="image/png"
+        )
+      img(:alt="$route.params.spaceObject" class="h-[170px] mx-auto mt-8 \
+      sm:h-[300px] sm:mt-[60px] xl:h-[445px]")
 
-  div(class="flex flex-col justify-center")
-    nav(class="h-7 mt-[26px] font-barlow-condensed text-sm leading-[16.8px] \
+  div(class="flex flex-col mt-[26px] sm:mt-[53px] xl:ml-[157px] xl:mt-[174px]")
+    nav(class="h-7 font-barlow-condensed text-sm leading-[16.8px] \
     tracking-[2.36px] text-slate-400 gap-[27px] flex justify-center \
-    sm:mt-[53px] sm:text-base sm:h-[34px] sm:leading-[19px]")
+    sm:text-base sm:h-[34px] sm:leading-[19px] sm:gap-[36px] xl:justify-start")
       router-link(
         v-for="(item, i) in ['MOON', 'MARS', 'EUROPA', 'TITAN']"
         :key="i"
--- src/views/destination/SpaceObjectView.vue
+++ src/views/destination/SpaceObjectView.vue
@@ -1,23 +1,27 @@
 <template lang="pug">
 
-div(class="flex flex-col items-center max-w-[327px] sm:max-w-[573px] mx-auto")
+div(class="flex flex-col items-center max-w-[327px] sm:max-w-[573px] mx-auto \
+xl:mx-0 xl:items-start xl:max-w-[445px]")
   h1(class="font-bellefair text-[56px] leading-[64px] text-white mt-5 \
-  sm:text-[80px] sm:leading-[92px] \
+  sm:text-[80px] sm:leading-[92px] sm:mt-8 xl:mt-[37px] xl:text-[100px] \
+  xl:leading-[115px] \
   ") {{ this.$route.params.spaceObject.toUpperCase() }}
 
   p(class="text-[15px] leading-[25px] text-center text-slate-400 \
-  mt-[1px] sm:mt-2 sm:text-base sm:leading-[28px] \
+  mt-[1px] sm:mt-2 sm:text-base sm:leading-[28px] xl:text-start xl:mt-[14px] \
+  xl:text-lg xl:leading-8 \
   ") {{ currentObject.description }}
 
   //- line
-  div(class="bg-[#383B4B] h-[1px] w-full mt-8 sm:mt-[49px]")
+  div(class="bg-[#383B4B] h-[1px] w-full mt-8 sm:mt-[49px] xl:mt-[54px]")
 
-  div(class="flex flex-col items-center mb-[58px] sm:flex-row sm:gap-[11px] \
-  sm:mb-[62px]")
+  div(class="flex flex-col items-center mb-[38px] sm:flex-row sm:gap-[11px] \
+  sm:mb-[62px] xl:flex-row xl:items-start xl:gap-3")
     div(
       v-for="(item, i) in distanceTravelData"
       :key="i"
-      class="flex flex-col items-center sm:w-[216px]"
+      class="flex flex-col items-center sm:w-[216px] xl:items-start \
+      xl:w-[209px]"
     )
       span(class="mt-8 font-barlow-condensed text-sm tracking-[2.36px] \
     leading-[16.8px] text-slate-400 sm:mt-7") {{ item[0] }}
