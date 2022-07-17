f1b07fb507cae451b16d423942a91c9d6c58ad79
ivan-f-sokolov <sokolovivanf@gmail.com>
2022-07-17 15:33:40 +0500

    Tablet crew styling

--- src/views/crew/CrewView.vue
+++ src/views/crew/CrewView.vue
@@ -6,7 +6,7 @@ div(class="xl:min-h-[calc(100vh-136px)]")
   xl:min-h-[calc(576px+136px)] xl:max-h-[788px]")
 
     div(class="xl:ml-[166px] xl:mb-[112px] xl:w-[509px] xl:h-[576px] xl:flex \
-    xl:justify-between xl:flex-col ")
+    xl:justify-between xl:flex-col sm:w-full")
 
       p(class="font-barlow-condensed text-white text-base leading-[19px] \
       tracking-[2.7px] flex justify-center sm:text-xl sm:leading-6 \
@@ -16,7 +16,8 @@ div(class="xl:min-h-[calc(100vh-136px)]")
         span(class="opacity-25 mr-[16px] font-bold sm:mr-[19px] xl:mr-7") 02
         span MEET YOUR CREW
 
-      picture(class="w-fit xl:self-end")
+      picture(class="w-full sm:absolute sm:bottom-0 xl:self-end overflow-hidden \
+      sm:h-[532px]")
         source(
           :srcset="require(`@/assets/crew/${$route.params.member}.webp`)"
           type="image/webp"
@@ -26,14 +27,14 @@ div(class="xl:min-h-[calc(100vh-136px)]")
           type="image/png"
           )
         img(:alt="$route.params.member" class="h-[223px] mx-auto mt-8 \
-        sm:h-[300px] sm:mt-[60px] xl:h-[445px]")
+        sm:h-full sm:mt-0 sm:m-auto xl:h-[445px]" :class="commanderClass")
 
     //- line
-    div(class="bg-[#383B4B] h-[1px] w-[327px] sm:mt-[49px] xl:mt-[54px]")
+    div(class="bg-[#383B4B] h-[1px] w-[327px] sm:hidden xl:mt-[54px]")
 
-    div(class="flex flex-col mt-8 sm:mt-[53px] xl:ml-[157px]")
+    div(class="flex flex-col mt-8 sm:mt-0 sm:flex-col-reverse xl:ml-[157px]")
 
-      nav(class="h-[10px] gap-2 flex m-auto")
+      nav(class="gap-2 flex m-auto")
         router-link(
           v-for="(item, i) in links"
           :key="i"
@@ -55,6 +56,13 @@ export default {
     return {
       links: data.crew.map((item) => item.role),
     };
+  },
+  computed: {
+    commanderClass() {
+      return {
+        "sm:!h-[572px]": this.$route.params.member === "commander",
+      };
+    },
   },
 };
 </script>
--- src/views/crew/MemberView.vue
+++ src/views/crew/MemberView.vue
@@ -1,20 +1,20 @@
 <template lang="pug">
 
-div(class="flex flex-col items-center max-w-[327px] sm:max-w-[573px] mx-auto \
-xl:mx-0 xl:items-start xl:max-w-[445px]")
+div(class="flex flex-col items-center max-w-[327px] sm:max-w-[458px] mx-auto \
+xl:mx-0 xl:items-start xl:max-w-[445px]" :class="widthClass")
 
-  h2(class="font-bellefair text-base leading-[18.34px] text-white mt-8 opacity-50 \
-  sm:text-[80px] sm:leading-[92px] sm:mt-8 xl:mt-[37px] xl:text-[100px] \
-  xl:leading-[115px] \
+  h2(class="font-bellefair text-base leading-[18.34px] text-white mt-8 \
+  opacity-50 sm:text-2xl sm:leading-[27.5px] sm:mt-[60px] xl:mt-[37px] \
+  xl:text-[100px] xl:leading-[115px] \
   ") {{ currentObject.role.split("-").join(" ").toUpperCase() }}
 
   h1(class="font-bellefair text-2xl leading-[28px] text-white mt-2 \
-  sm:text-[80px] sm:leading-[92px] sm:mt-8 xl:mt-[37px] xl:text-[100px] \
+  sm:text-[40px] sm:leading-[45.8px] sm:mt-2 xl:mt-[37px] xl:text-[100px] \
   xl:leading-[115px] \
   ") {{ currentObject.name.split("-").join(" ").toUpperCase() }}
 
-  p(class="text-[15px] leading-[25px] mb-[30px] text-center text-slate-400 \
-  mt-4 sm:mt-2 sm:text-base sm:leading-[28px] xl:text-start xl:mt-[14px] \
+  p(class="text-[15px] leading-[25px] mb-[26px] text-center text-slate-400 \
+  mt-4 sm:mt-4 sm:mb-[40px] sm:text-base sm:leading-[28px] xl:text-start xl:mt-[14px] \
   xl:text-lg xl:leading-8 \
   ") {{ currentObject.bio }}
 
@@ -34,6 +34,13 @@ export default {
       return this.crew.find(
         (object) => object.role === this.$route.params.member
       );
+    },
+    widthClass() {
+      return {
+        "sm:max-w-[520px]": this.$route.params.member === "mission-specialist",
+        "sm:max-w-[592px]": this.$route.params.member === "pilot",
+        "sm:max-w-[536px]": this.$route.params.member === "flight-engineer",
+      };
     },
   },
 };
